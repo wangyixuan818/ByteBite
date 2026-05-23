5431d5833271a9ceeee7c0566709d8f2157804af
He <helen07070409@gmail.com>
2026-05-23 22:03:53 +0800

    front end set up

---
+++ frontend/app/routes/home.tsx
@@ -0,0 +1,15 @@
+import type { Route } from "./+types/home";
+import { Welcome } from "../welcome/welcome";
+
+export function meta({}: Route.MetaArgs) {
+  return [
+    { title: "New React Router App" },
+    { name: "description", content: "Welcome to React Router!" },
+  ];
+}
+
+export default function Home() {
+  return (
+    <h1>hello world</h1>
+  );
+}
