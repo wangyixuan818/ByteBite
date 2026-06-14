1de575b718947de1e3f5d4befeba73ed8761115f
Wang Yixuan <wangyixuan818@gmail.com>
2026-06-14 18:31:55 +0800

    fixed minor bugs here and there and set up jest config file

--- server/db.js
+++ server/db.js
@@ -1,8 +1,5 @@
 const { Pool, types } = require('pg');               
 
-// parse timestamptz as string to avoid timezone issues
-types.setTypeParser(1114, str => str); 
-
 const pool = new Pool({                        
   connectionString: process.env.DATABASE_URL,  
   ssl: { rejectUnauthorized: false },         
--- server/jest.config.js
+++ server/jest.config.js
@@ -1,7 +1,7 @@
 module.exports = {
    testEnvironment: 'node',
    setupFiles: ['<rootDir>/__tests__/setup.js'],
-   testMatch: ['**/__tests__/**/*.test.js'],
+   testMatch: ['**/__tests__/unit/**/*.test.js', '**/__tests__/integration/**/*.test.js'],
    testPathIgnorePatterns: ['/node_modules/', '/__tests__/setup.js', '/__tests__/helpers/'],
    maxWorkers: 1, // run test files one at a time --> cuz there's only one db
 }
