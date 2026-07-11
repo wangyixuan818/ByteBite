b7a7bb3b8adf156fad3c009e672e1e5e4b73d690
Wang Yixuan <wangyixuan818@gmail.com>
2026-07-11 21:39:00 +0800

    removed duplicate code after catalog.js was added

--- server/__tests__/helpers/auth.js
+++ server/__tests__/helpers/auth.js
@@ -22,36 +22,4 @@ async function signupAndGetToken(displayName = 'Test User') {
     return { token: res.body.token, user: res.body.user, householdId: hh.rows[0].household_id };
 }
 
-// inserts known catalog rows for tests, returns the new id so tests can reference them
-async function insertCategory({ name, default_storage = 'fridge',
-                                pantry_days = null, fridge_days = null, freezer_days = null }) {
-    const res = await pool.query(
-        `INSERT INTO categories (name, default_storage, pantry_days, fridge_days, freezer_days)
-         VALUES ($1, $2, $3, $4, $5) RETURNING id`,
-        [name, default_storage, pantry_days, fridge_days, freezer_days]
-    );
-    return res.rows[0].id;
-}
-
-async function insertFoodType({ name, category_id = null, default_storage = null,
-                                pantry_days = null, fridge_days = null, freezer_days = null }) {
-    const res = await pool.query(
-        `INSERT INTO food_types (name, category_id, default_storage, pantry_days, fridge_days, freezer_days)
-         VALUES ($1, $2, $3, $4, $5, $6) RETURNING id`,
-        [name, category_id, default_storage, pantry_days, fridge_days, freezer_days]
-    );
-    return res.rows[0].id;
-}
-
-async function insertBrandProduct({ brand, food_type_id, default_storage = null,
-                                    pantry_days = null, fridge_days = null, freezer_days = null }) {
-    const res = await pool.query(
-        `INSERT INTO brand_products (brand, food_type_id, default_storage, pantry_days, fridge_days, freezer_days)
-         VALUES ($1, $2, $3, $4, $5, $6) RETURNING id`,
-        [brand, food_type_id, default_storage, pantry_days, fridge_days, freezer_days]
-    );
-    return res.rows[0].id;
-}
-
-module.exports = { insertCategory, insertFoodType, insertBrandProduct };
 module.exports = { signupAndGetToken };
